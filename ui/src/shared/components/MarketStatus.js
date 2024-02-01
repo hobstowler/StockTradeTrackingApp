5f7bca8ad66a7c4d404fb7d78a56761e03a3c63a
Hobs Towler <91224707+hobstowler@users.noreply.github.com>
2024-02-01 10:23:25 -0500

    add market status components and endpoint

---
+++ ui/src/shared/components/MarketStatus.js
@@ -0,0 +1,40 @@
+import {useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {getClock} from "../../application/actions";
+import {Box, Container, useTheme} from "@mui/system";
+
+const MarketStatus = () => {
+  const clock = useSelector(({application}) => application?.clock)
+  const dispatch = useDispatch()
+  const theme = useTheme()
+
+  useEffect(() => {
+      dispatch(getClock())
+  }, [])
+
+  const formatStatus = (marketStatus) => {
+    switch (marketStatus) {
+      case 'premarket':
+        return <Box sx={{color: theme.palette.success.light, display: 'inline', fontWeight: 600}}>Pre Market</Box>
+      case 'open':
+        return <Box sx={{color: theme.palette.success.main, display: 'inline', fontWeight: 600}}>Open</Box>
+      case 'postmarket':
+        return <Box sx={{color: theme.palette.success.dark, display: 'inline', fontWeight: 600}}>Post Market</Box>
+      case 'closed':
+        return <Box sx={{color: theme.palette.warning.main, display: 'inline', fontWeight: 600}}>Closed</Box>
+      default:
+        return marketStatus
+    }
+  }
+
+  return (
+    <Container className='MarketStatus'>
+      <Box sx={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', mx: '24px', my: '8px'}}>
+        <Box>{`Market status: `}{formatStatus(clock?.state)}</Box>
+        <Box>{`Market will be `}{formatStatus(clock?.next_state)}{` at ${clock?.next_change}`}</Box>
+      </Box>
+    </Container>
+  )
+}
+
+export default MarketStatus
