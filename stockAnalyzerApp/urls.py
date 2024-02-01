e0c10470f13850ba42417339aab596ee50aa72ce
Hobs Towler <91224707+hobstowler@users.noreply.github.com>
2024-02-01 10:28:21 -0500

    endpoints for clock and stock lookup/search

--- stockAnalyzerApp/urls.py
+++ stockAnalyzerApp/urls.py
@@ -1,5 +1,9 @@
 from django.urls import include, path
+from .views import *
 
 urlpatterns = [
-
+    path('clock/', clock, name='get clock'),
+    path('lookup/', symbol_lookup, name='symbol lookup'),
+    path('lookup/<security_type>', symbol_lookup, name='symbol lookup'),
+    path('search/', symbol_search, name='symbol search')
 ]
