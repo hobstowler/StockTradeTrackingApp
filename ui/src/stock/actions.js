d015899ef30e66f6c6b25623303f1f3bc118da85
Hobs Towler <91224707+hobstowler@users.noreply.github.com>
2024-02-14 09:39:45 -0500

    watch list actions

--- ui/src/stock/actions.js
+++ ui/src/stock/actions.js
@@ -44,3 +44,34 @@ export const symbolSearch = (term) => (dispatch, state, _) => {
 export const clearSymbols = () => (dispatch, state, _) => {
   dispatch({type: 'CLEAR_SYMBOL'})
 }
+
+export const setActiveGroup = (groupName) => (dispatch, state, _) => {
+  dispatch({type: 'SET_ACTIVE_WATCH_GROUP', 'groupName': groupName})
+}
+
+export const refreshActiveWatchList = (groupName, refresh = false) => (dispatch, state, _) => {
+  if (!refresh) {
+    dispatch({type: 'LOAD_WATCHLIST_START'})
+  }
+  const symbols = state().stock.watchList.groups?.[groupName].symbols.map((symbol) => {return symbol.symbol})
+  fetch(`api/search?q=${symbols.toString()}&greeks=false`)
+    .then(async response => {
+      const hasJson = response.headers.get('content-type')?.includes('application/json')
+      const json = hasJson ? await response.json() : null
+
+      if (!response.ok) {
+        let error = (json && json.error) || response.status
+        dispatch({type: 'UPDATE_WATCHLIST_ERROR', error: error})
+        return
+      }
+
+      let quotes = json?.quotes?.quote
+      if (typeof quotes === 'object' && !Array.isArray(quotes)) {
+        quotes = [quotes]
+      } else {
+        dispatch({type: 'UPDATE_WATCHLIST_ERROR', error: 'error'})
+      }
+
+      dispatch({type: 'UPDATE_WATCHLIST', loaded: true, groupName: groupName, quotes: quotes})
+    })
+}
