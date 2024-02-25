1bcd4a69932d86068eac5079ba298e2643ed1b05
Hobs Towler <91224707+hobstowler@users.noreply.github.com>
2024-02-25 12:47:51 -0500

    more lambda functions

---
+++ api/src/handlers/GetWatchList.py
@@ -0,0 +1,10 @@
+def get_watch_list(event, context):
+
+    return {
+        "statusCode": 200,
+        'headers': {
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': "*",
+            'Access-Control-Allow-Methods': 'GET'
+        },
+    }
